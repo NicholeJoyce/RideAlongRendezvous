7eab00500f7d92eff95e0b07ec63ac58fc8400b8
NicholeJoyce <santosng@students.nu-baliwag.edu.ph>
2023-05-09 00:35:08 +0800

    connection for hosting

--- includes/connection.php
+++ includes/connection.php
@@ -1,16 +1,16 @@
 <?php
 
-$server = "localhost";
-$server_username = "root";
-$server_password = "";
-$server_database = "carpool";
-$home = "http://localhost/trylang/";
-
 // $server = "localhost";
-// $server_username = "u235219407_jeyymsantos";
-// $server_password = "Jeyym@15";
-// $server_database = "u235219407_carpool_app";
-// $home = "https://carpool.jeyymsantos.com/";
+// $server_username = "root";
+// $server_password = "";
+// $server_database = "carpool";
+// $home = "http://localhost/trylang/";
+
+$server = "localhost";
+$server_username = "u235219407_ridealong";
+$server_password = "RideAlongRendezvous01*";
+$server_database = "u235219407_ridealong";
+$home = "https://ridealong.dnails.shop//";
 
 $connection = mysqli_connect($server, $server_username, $server_password, $server_database);
 session_start();
