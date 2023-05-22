0d6905f982675f1afd6603c8f8553dce7065f245
NicholeJoyce <santosng@students.nu-baliwag.edu.ph>
2023-05-23 02:28:32 +0800

    report nalanggg huhu

--- includes/connection.php
+++ includes/connection.php
@@ -1,17 +1,17 @@
 <?php
 //localhost
-$server = "localhost";
-$server_username = "root";
-$server_password = "";
-$server_database = "carpool";
-$home = "http://localhost/trylang/";
+// $server = "localhost";
+// $server_username = "root";
+// $server_password = "";
+// $server_database = "carpool";
+// $home = "http://localhost/trylang/";
 
 //hosting
-// $server = "localhost";
-// $server_username = "u235219407_ridealong";
-// $server_password = "RideAlongRendezvous01*";
-// $server_database = "u235219407_ridealong";
-// $home = "https://ridealong.dnails.shop//";
+$server = "localhost";
+$server_username = "u235219407_ridealong";
+$server_password = "RideAlongRendezvous01*";
+$server_database = "u235219407_ridealong";
+$home = "https://ridealong.dnails.shop//";
 
 $connection = mysqli_connect($server, $server_username, $server_password, $server_database);
 session_start();
